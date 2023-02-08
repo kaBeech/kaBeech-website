58d34454fffc9116662b060575e1e76fbba94263
kaBeech <contact@kaBeech.com>
2023-02-08 02:08:25 -0800

    Cleanup

--- qwik-app/src/components/beechi/beechi.tsx
+++ qwik-app/src/components/beechi/beechi.tsx
@@ -3,7 +3,7 @@ import { component$ } from "@builder.io/qwik";
 export const Beechi = component$(() => {
   return (
     <div class="invertColor roundCornersTop">
-      <img src="../../../public/beechi.svg" alt="Beechi, kaBeech's avatar" />
+      <img src="/beechi.svg" alt="Beechi, kaBeech's avatar" />
     </div>
   );
 });
