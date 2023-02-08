0a3f635bc20d70abb64a6e6750e404d78798f3f1
kaBeech <contact@kaBeech.com>
2023-02-07 21:49:40 -0800

    Add test buttons to responseBar

--- qwik-app/src/components/responseBar/responseBar.tsx
+++ qwik-app/src/components/responseBar/responseBar.tsx
@@ -1,12 +1,20 @@
 import { component$ } from "@builder.io/qwik";
 import { ResponseButton } from "../responseButton/responseButton";
 
+interface TestData {
+  response: string;
+}
+
 export const ResponseBar = component$(() => {
+  const testData: TestData[] = [{ response: "quiet" }, { response: "LOOUD" }];
+
   return (
     <div class="invertColor flex spaceBetween">
       <div>{"<"}</div>
       <div>
-        <ResponseButton response="Test" />
+        {testData.map((i) => (
+          <ResponseButton response={i.response} />
+        ))}
       </div>
       <div>&gt;</div>
     </div>
