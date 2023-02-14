697ebf1a1d7b2f9382fe1ed203c7124b99dc05ee
kaBeech <contact@kaBeech.com>
2023-02-13 16:09:51 -0800

    Update About Me page

--- qwik-app/src/routes/about-me/index.tsx
+++ qwik-app/src/routes/about-me/index.tsx
@@ -1,5 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { Link } from "@builder.io/qwik-city";
 import { Beechy } from "~/components/beechy/beechy";
 import { ResponseBar } from "~/components/responseBar/responseBar";
 
@@ -9,14 +10,34 @@ export default component$(() => {
       <Beechy />
       <div class="screenContents">
         <h1>About Me</h1>
-        <p></p>
+        <p>
+          My name's Kyle Beechly and I'm a software engineer from the Pacific
+          Northwest (in America). I built this site to showcase some of my work
+          in the new{" "}
+          <Link class="link" href="/poqd">
+            POQD stack
+          </Link>
+        </p>
+        <p>
+          If you're interested, I prefer{" "}
+          <Link class="link" href="/about-me/pronouns">
+            xe/xyr or ze/zyr pronouns
+          </Link>
+          . In general I'm not very strict about it though, as long as you're
+          being kind ♡
+        </p>
+        <p>
+          When I'm not coding you can probably find me running, building fantasy
+          worlds for tabletop roleplaying games, SHREDDING guitar, and/or
+          thinking deeply about the nature of being
+        </p>
       </div>
       <ResponseBar
         responses={[
           {
             responseShort: "Tech",
-            responseLong: "Yeah, gimme all your geeky tech credentials!",
-            linkPath: "/about-me/tech",
+            responseLong: "Wait, what's all this about a poked stack?",
+            linkPath: "/poqd",
           },
           {
             responseShort: "Pronouns",
