be9e7516eb3943dbbd5d66c201aea3c31aea40e0
kaBeech <contact@kaBeech.com>
2023-02-14 16:47:00 -0800

    Add projects info

--- qwik-app/src/routes/projects/index.tsx
+++ qwik-app/src/routes/projects/index.tsx
@@ -1,16 +1,56 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { DocumentHead, Link } from "@builder.io/qwik-city";
 import { Beechy } from "~/components/beechy/beechy";
 import { ResponseBar } from "~/components/responseBar/responseBar";
 import { linkTiles } from "~/util/linkTiles";
+import styles from "./projects.css?inline";
 
 export default component$(() => {
+  useStylesScoped$(styles);
+
   return (
     <div class="screenContainer">
       <Beechy />
       <div class="screenContents">
         <h1>Projects</h1>
-        <p></p>
+        <p>Here are some cool things I've made on teams with other people!</p>
+        <h3>
+          Scroll down or use the response buttons below to check them out!
+        </h3>
+        <h2>Audiopile</h2>
+        <img
+          src="/waveform.png"
+          alt="The Audiopile logo - overlapping multicolored sine waves"
+        />
+        <button>
+          <Link href="https://www.audiopile.cloud/">Check out Audiopile!</Link>
+        </button>
+        <p>A simple, professional tool for managing audio in the cloud</p>
+        <p>
+          A question for all the musicians, podcasters, and other audio
+          professionals of the world: How often do you want to privately share a
+          piece of audio with specific people?
+        </p>
+        <p>
+          Maybe it's a completed but unreleased track you're sending to a client
+          for final approval. Maybe it's a recording of just your voice that
+          you're sending to your collaborators to mix in to a final project. Or
+          it's a project you've been working on that you're sending to a friend
+          with fresh ears for an opinion. Heck, maybe it's just a tune that came
+          to you in the shower that you recorded on your shower phone (people
+          have those, right?) and want to listen to later on your studio
+          computer.
+        </p>
+        <p>
+          In any case, do you know how to get that audio from wherever it is now
+          to wherever it needs to get to in a form that can be easily used?
+        </p>
+        <p>
+          Now you do:{" "}
+          <Link class="link" href="https://www.audiopile.cloud/">
+            Audiopile
+          </Link>
+        </p>
       </div>
 
       <ResponseBar
