834ac9ae55f04e8f5d2d6b616dfad542d06f4cd5
kaBeech <contact@kaBeech.com>
2023-02-15 23:23:20 -0800

    Adjust button styling

--- qwik-app/src/components/responseDetail/responseDetail.tsx
+++ qwik-app/src/components/responseDetail/responseDetail.tsx
@@ -20,13 +20,13 @@ export const ResponseDetail = component$((props: ResponseDetailProps) => {
       <div class="lazyColor flex">
         <div class="responseText">{props.response.fullMessage}</div>
         <div class="flex tinyColumn gapHalf">
-          <button class="lazyButton flex">
-            <Link href={props.response.linkTile.linkPath} class="flex width100">
+          <Link href={props.response.linkTile.linkPath} class="flex width100">
+            <button class="lazyButton flex justifyCenter">
               <img src="/icons/check.svg" alt="Confirm response" />
-            </Link>
-          </button>
+            </button>
+          </Link>
           <button
-            class="lazyCancel flex"
+            class="lazyCancel flex justifyCenter"
             onClick$={() => {
               hideResponseDetail();
             }}
