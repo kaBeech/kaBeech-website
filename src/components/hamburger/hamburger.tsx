5343d12f3637180205bc78f02c5551abb29639fb
kaBeech <contact@kaBeech.com>
2023-04-13 22:08:30 -0700

    Replace image links with text on mobile

--- src/components/hamburger/hamburger.tsx
+++ src/components/hamburger/hamburger.tsx
@@ -31,7 +31,7 @@ export const Hamburger = component$((props: HamburgerProps) => {
         src="/icons/menu.svg"
         alt="Menu Button"
       />
-      {state.selected && (
+      {state.selected && window.outerWidth >= 1000 && (
         <>
           <Link class="link" href="/">
             <img class="lazy" src="/icons/home.svg" alt="Home" />
@@ -52,7 +52,7 @@ export const Hamburger = component$((props: HamburgerProps) => {
           </Link>
           <Link class="link" href="/tech">
             <img class="lazy" src="/icons/cpu.svg" alt="Technology" />
-          </Link>{" "}
+          </Link>
           <Link class="link" href="/projects">
             <img class="lazy" src="/icons/hammer-outline.svg" alt="Projects" />
           </Link>
@@ -60,6 +60,28 @@ export const Hamburger = component$((props: HamburgerProps) => {
             <img class="lazy" src="/icons/mail.svg" alt="Contact Me" />
           </Link>
         </>
+      )}
+      {state.selected && window.outerWidth < 1000 && (
+        <div class="shadow">
+          <Link class="link" href="/">
+            <div class="blink">Home</div>
+          </Link>
+          <Link class="link" href="/fun">
+            <div class="blink">Fun and Games</div>
+          </Link>
+          <Link class="link" href="/about-me">
+            <div class="blink">About Me</div>
+          </Link>
+          <Link class="link" href="/tech">
+            <div class="blink">Technology</div>
+          </Link>
+          <Link class="link" href="/projects">
+            <div class="blink">Projects</div>
+          </Link>
+          <Link class="link" href="/contact">
+            <div class="blink">Contact Me</div>
+          </Link>
+        </div>
       )}
     </div>
   );
