828040cfd1d079d27c13ee30bf4b0caac9aa23dc
kaBeech <contact@kaBeech.com>
2023-05-15 14:02:44 -0700

    Add WASM info

--- src/routes/tech/wasm/index.tsx
+++ src/routes/tech/wasm/index.tsx
@@ -12,7 +12,51 @@ export default component$(() => {
       <div class="screenContents">
         <h1>WebAssembly (WASM)</h1>
         <h2 class="responseTextLight">What is WebAssembly?</h2>
-        <p>Content coming soon!</p>
+        <p>
+          WebAssembly, or WASM, is a low-level programming language designed to
+          be executed by web browsers
+        </p>
+        <h2 class="responseTextLight">
+          Okay, you lost me again. Can you say that in human words?
+        </h2>
+        <p>
+          Absolutely! But first let's talk about low-level and high-level
+          programming languages
+        </p>
+        <p>
+          Matter of fact, let's talk about my one-sentence answer the the
+          question "What is WebAssembly?" This will be our example, because it's
+          written in a way very similar to how we use high-level programming
+          languages. Here it is again: "WebAssembly, or WASM, is a low-level
+          programming language designed to be executed by web browsers"
+        </p>
+        <p>
+          A high-level language assumes a level of knowlege of the person
+          reading it. In the case of our WASM sentence, I assume quite a lot
+          about you, dear reader. I assume you know what a low-level language
+          is, and how it contrasts with a high-level one. I assume you know what
+          it means for code to be executed, and why it's interesting that WASM
+          can be executed by web browsers. I assume you know what{" "}
+          <Link class="link">programming languages</Link> and{" "}
+          <Link class="link">web browsers</Link> are. I assume that you can read
+          my particular English <Link class="link">ideolect</Link>, that you can
+          infer that "WASM" is an abbreviation for "Web Assembly," and that
+          you'll pronounce it like /'wasm̩/ and not like "W-A-S-M"
+        </p>
+        <p>
+          However, some or all of that may be untrue, and you may be left
+          wondering{" "}
+          <span class="link">
+            "Who is this maniac, and why is xe making up so many weird words? I
+            can make up words too, you know. Yupt!"
+          </span>
+        </p>
+        <p>...which is why we have low-level programming languages!</p>
+        <p>...</p>
+        <p>
+          WebAssembly is named that because it is an (ital: Assembly Language)
+          for the (ital: Web)
+        </p>
         <h2 class="responseTextLight">Why would I want to use WebAssembly?</h2>
         <p>Content coming soon!</p>
         <h2 class="responseTextLight">Where can I use WebAssembly?</h2>
