cf027b5607e8fbf59b1dc8c660e81fe4c68297df
kaBeech <contact@kaBeech.com>
2023-05-15 21:07:30 -0700

    Add WASM info

--- src/routes/tech/wasm/index.tsx
+++ src/routes/tech/wasm/index.tsx
@@ -44,10 +44,10 @@ export default component$(() => {
         <p>
           A high-level language assumes a level of knowlege of the person
           reading it. In the case of our WASM sentence, I assume quite a lot
-          about you, dear reader. I assume you know what a low-level language
-          is, and how it contrasts with a high-level one. I assume you know what
-          it means for code to be executed, and why it's interesting that WASM
-          can be executed by web browsers. I assume you know what{" "}
+          about you, dear reader. I assume you know what a low-level language is
+          and how it contrasts with a high-level one. I assume you know what it
+          means for code to be executed and why it's interesting that WASM can
+          be executed by web browsers. I assume you know what{" "}
           <Link class="link">programming languages</Link> and{" "}
           <Link class="link">web browsers</Link> are. I assume that you can read
           my particular <Link class="link">ideolect</Link> of English, that
@@ -77,14 +77,14 @@ export default component$(() => {
         </p>
         <p>
           Given the added length, you might conclude that using low-level
-          language would make it take more time to convey an idea. However, this
-          is not always the case
+          language would take more time to convey an idea. However, this is not
+          always the case
         </p>
         <p>
           Imagine someone reading our one-sentence answer and not knowing what a
           'low-level programming language' is. Let's call this person 'Kyle.' By
           xemself, xe can sit and reread the sentence, but without further
-          information xe might not get much closer to understanding it. xe could
+          information xe might not get much closer to understanding it. Xe could
           guess what it means, but this could lead to unpredictable results
         </p>
         <p>
@@ -113,7 +113,7 @@ export default component$(() => {
           a way xe will understand. Then, Beechy will have to actually convey
           that information to Kyle, probably stopping to answer some of xis
           questions along the way. This whole process, from Kyle reading the
-          sentence until xe finally understands it, takes a lot of time
+          sentence until xe finally understands it, takes a long time
         </p>
         <p>
           ...and this is all assuming that Beechy interprets what I mean
@@ -128,7 +128,40 @@ export default component$(() => {
           Okay, I'm with you now. But... weren't we talking about computers or
           something?
         </h3>
-        <p>...</p>
+        <p>Right you are!</p>
+        <p>
+          Computers, once you follow the chain of technology down to a low
+          enough level (see what I did there?), only understand low-level
+          programming languages. If you feed your computer's CPU a string of
+          text that says "Hey, could you print me something that says 'Hello
+          World,' but in all caps please?" you're likely to receive a response
+          like this:
+        </p>
+        <p class="bin">
+          "
+          <Link class="link bin" href="https://lingojam.com/BinaryTranslator">
+            01011001011011110010110000100000010010010010011101101101001000000110001001100001011100110110100101100011011000010110110001101100011110010010000001100001001000000111001101101111011101010111000001100101011001000010110101110101011100000010000001100011011000010110110001100011011101010110110001100001011101000110111101110010001011000010000001001001001000000110100001100001011101100110010100100000011011100110111100100000011010010110010001100101011000010010000001110111011010000110000101110100001000000111100101101111011101010010011101110010011001010010000001110011011000010111100101101001011011100110011100100000011100100110100101100111011010000111010000100000011011100110111101110111
+          </Link>
+          "
+        </p>
+        <p>
+          Well... not exactly. Although we often consider binary code (which is
+          what all those 1's and 0's are) as the classic low-level language that
+          machines think in, modern machines are a bit more complicated. These
+          days, most computers use hexadecimal code (which uses the numbers 0-9
+          as well as the letters a-f) to communicate
+        </p>
+        <p>
+          Regardless of whether they use binary or hexidecimal characters, these
+          characters must have a system built around them that makes it clear
+          what "010110010110" actually means. These systems that organize
+          characters in ways that machines can understand are called 'assembly
+          languages'
+        </p>
+        <h3>
+          An 'Assembly Language' is a low-level programming language that a
+          machine can understand
+        </h3>
         <p>
           WebAssembly is named that because it is an{" "}
           <span class="ital">Assembly Language </span>for the
