535c76c6dfca59cb7f9475b0db0131b546c7fe5c
kaBeech <contact@kaBeech.com>
2023-05-26 20:27:01 -0700

    Cleanup

--- src/routes/fun/al-lugha-misma/challenge/index.tsx
+++ src/routes/fun/al-lugha-misma/challenge/index.tsx
@@ -1,28 +1,11 @@
 import { component$, useStylesScoped$ } from "@builder.io/qwik";
 import type { DocumentHead, RequestEvent } from "@builder.io/qwik-city";
-import { routeLoader$ } from "@builder.io/qwik-city";
 import { Link } from "@builder.io/qwik-city";
 import { Beechy } from "~/components/beechy/beechy";
 import { ResponseBar } from "~/components/responseBar/responseBar";
 import { linkTiles } from "~/util/linkTiles";
 import styles from "../al-lugha-misma.css?inline";
 import { AlLughaMismaTable } from "~/components/alLughaMismaTable/alLughaMismaTable";
-
-interface TranslatedWord {
-  reference_word_english: string;
-  transliterated_word: string;
-  language: string;
-}
-
-// interface TranslationByReferenceWord {
-//   referenceWord: string;
-//   translatedWords: string[];
-// }
-
-interface TranslationByLanguage {
-  language: string;
-  translatedWords: string[];
-}
 
 let alLughaMismaAPI: string;
 
@@ -35,71 +18,8 @@ export const onGet = (requestEvent: RequestEvent) => {
   }
 };
 
-export const useGetTranslatedWordList = routeLoader$(async () => {
-  const res = await fetch(
-    `${alLughaMismaAPI}/word_list/Colors/languages/Arabic,English,French,Hawaiian,Hindi,Indonesian,PigLatin,Spanish,Swahili/key`
-  );
-  const data = await res.json();
-  const translatedWordList: TranslatedWord[] = [];
-  data.challenge_key.forEach((translatedWord: TranslatedWord) =>
-    translatedWordList.push(translatedWord)
-  );
-  return (translatedWordList || "Error") as TranslatedWord[];
-});
-
 export default component$(() => {
   useStylesScoped$(styles);
-  const translatedWordList = useGetTranslatedWordList().value;
-  const languages: string[] = [];
-  const referenceWords: string[] = [];
-  const translationsByLanguage: TranslationByLanguage[] = [];
-  // const translationsByReferenceWord: TranslationByReferenceWord[] = [];
-
-  for (const translatedWord of translatedWordList) {
-    if (!languages.includes(translatedWord.language)) {
-      languages.push(translatedWord.language);
-    }
-    if (!referenceWords.includes(translatedWord.reference_word_english)) {
-      referenceWords.push(translatedWord.reference_word_english);
-    }
-  }
-
-  for (const language of languages) {
-    translationsByLanguage.push({
-      language: language,
-      translatedWords: [],
-    });
-  }
-
-  for (const translationByLanguage of translationsByLanguage) {
-    for (const translatedWord of translatedWordList) {
-      if (translatedWord.language === translationByLanguage.language) {
-        translationByLanguage.translatedWords.push(
-          translatedWord.transliterated_word
-        );
-      }
-    }
-  }
-
-  // for (const referenceWord of referenceWords) {
-  //   translationsByReferenceWord.push({
-  //     referenceWord: referenceWord,
-  //     translatedWords: [],
-  //   });
-  // }
-
-  // for (const translationByReferenceWord of translationsByReferenceWord) {
-  //   for (const translatedWord of translatedWordList) {
-  //     if (
-  //       translatedWord.referenceWordEnglish ===
-  //       translationByReferenceWord.referenceWord
-  //     ) {
-  //       translationByReferenceWord.translatedWords.push(
-  //         translatedWord.transliteratedWord
-  //       );
-  //     }
-  //   }
-  // }
 
   return (
     <div class="screenContainer">
