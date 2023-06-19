2914f7e99104738d1dd0e8c39510aeaceafc4738
kaBeech <contact@kaBeech.com>
2023-06-19 12:49:38 -0700

    Add date picker

--- src/routes/fun/star-crossed/index.tsx
+++ src/routes/fun/star-crossed/index.tsx
@@ -1,128 +1,185 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead, RequestEvent } from "@builder.io/qwik-city";
-import { Link, routeLoader$ } from "@builder.io/qwik-city";
+import {
+  Resource,
+  component$,
+  useResource$,
+  useStore,
+  useStylesScoped$,
+} from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import { Link } from "@builder.io/qwik-city";
 import { Beechy } from "~/components/beechy/beechy";
 import { ResponseBar } from "~/components/responseBar/responseBar";
 import { linkTiles } from "~/util/linkTiles";
+import styles from "./star-crossed.css?inline";
 
-let starCrossedAPI: string;
+const starCrossedAPI = "https://hungry-heron-21.deno.dev";
 
-export const onGet = (requestEvent: RequestEvent) => {
-  const response = requestEvent.env.get("STARCROSSED_API");
-  if (response != undefined) {
-    starCrossedAPI = response;
-  } else {
-    console.error("STARCROSSED_API string not found upon request");
-  }
-};
-
-export const useStarCrossedTest = routeLoader$(async () => {
-  const res = await fetch(
-    `${starCrossedAPI}/star-crossings/1948-8-11,1952-3-3`
-  );
-  const data = await res.json();
-  let closestStarShownName: string;
-  if (data.closestStarCommonName !== "null") {
-    closestStarShownName = data.closestStarCommonName;
-  } else {
-    closestStarShownName = data.closestStarName;
-  }
-  return (
-    {
-      coordinates: data.coordinates,
-      infoURL: data.infoURL,
-      skyMapURL: data.skyMapURL,
-      closestStarName: data.closestStarName,
-      closestStarCommonName: data.closestStarCommonName,
-      closestStarShownName,
-      birthdays: ["1948-8-11", "1952-3-3"],
-    } || `Error - URI is ${starCrossedAPI}/star-crossings/1948-8-11,1952-3-3`
-  );
-});
+const birthday1 = "1999-08-11";
+const birthday2 = "2000-03-03";
 
 export default component$(() => {
-  const starCrossedTest = useStarCrossedTest();
+  useStylesScoped$(styles);
+  const state = useStore({
+    birthday1,
+    birthday2,
+    starCrossedAPI,
+  });
+
+  const starCrossedResource = useResource$<any>(async ({ track, cleanup }) => {
+    const birthday1 = track(() => state.birthday1);
+    const birthday2 = track(() => state.birthday2);
+    const abortController = new AbortController();
+    cleanup(() => abortController.abort("cleanup"));
+    const res = await fetch(
+      `${starCrossedAPI}/star-crossings/${birthday1},${birthday2}`,
+      {
+        signal: abortController.signal,
+      }
+    );
+    const data = await res.json();
+    let closestStarShownName: string;
+    if (data.closestStarCommonName !== "null") {
+      closestStarShownName = data.closestStarCommonName;
+    } else {
+      closestStarShownName = data.closestStarName;
+    }
+    return (
+      {
+        coordinates: data.coordinates,
+        infoURL: data.infoURL,
+        skyMapURL: data.skyMapURL,
+        closestStarName: data.closestStarName,
+        closestStarCommonName: data.closestStarCommonName,
+        closestStarShownName,
+        birthdays: ["1948-08-11", "1952-03-3"],
+      } || `Error - URI is ${starCrossedAPI}/star-crossings/1948-8-11,1952-3-3`
+    );
+  });
   return (
     <div class="screenContainer">
       <Beechy />
       <div class="screenContents">
         <h1>StarCrossed</h1>
+        <input
+          type="date"
+          onInput$={(ev: any) => (state.birthday1 = ev.target.value)}
+          value={birthday1}
+        />
+        <input
+          type="date"
+          onInput$={(ev: any) => (state.birthday2 = ev.target.value)}
+          value={birthday2}
+        />
         <h2>
-          Birthdays: {starCrossedTest.value.birthdays[0]} x{" "}
-          {starCrossedTest.value.birthdays[1]}
+          Birthdays: {state.birthday1} x {state.birthday2}
         </h2>
-        <h2>
-          StarCrossed Coordinates:{" "}
-          <strong>
-            {starCrossedTest.value.coordinates[0][0]}h{" "}
-            {starCrossedTest.value.coordinates[0][1]}m{" "}
-            {starCrossedTest.value.coordinates[0][2]}s,{" "}
-            {starCrossedTest.value.coordinates[1][0]}°{" "}
-            {starCrossedTest.value.coordinates[1][1]}′{" "}
-            {starCrossedTest.value.coordinates[1][2]}″
-          </strong>
-        </h2>
-        <h3>Closest Star: {starCrossedTest.value.closestStarShownName}</h3>
-        <p>
-          <Link
-            target="_blank"
-            rel="noopener noreferrer"
-            class="link margin1"
-            href={starCrossedTest.value.infoURL}
-          >
-            Learn more about {starCrossedTest.value.closestStarShownName}!
-          </Link>
-        </p>
-        <p>
-          <Link
-            target="_blank"
-            rel="noopener noreferrer"
-            class="link margin1"
-            href={`https://hungry-heron-21.deno.dev/star-crossings/1948-8-11,1952-3-3`}
-          >
-            Visit the StarCrossed API
-          </Link>
-        </p>
-        <p>
-          <Link
-            target="_blank"
-            rel="noopener noreferrer"
-            class="link margin1"
-            href={`https://github.com/kaBeech/star-crossed/tree/main#readme`}
-          >
-            View README on GitHub
-          </Link>
-        </p>
-        <h3>
-          This is what it looks like in your own special corner of the universe:
-        </h3>
-        <p>
-          If the iframe below does not display, please allow insecure content in
-          your browser or{" "}
-          <Link
-            target="_blank"
-            rel="noopener noreferrer"
-            class="link"
-            href={`http://www.wikisky.org/${starCrossedTest.value.skyMapURL.slice(
-              36
-            )}`}
-          >
-            click here to view the interactive map on WikiSky
-          </Link>
-        </p>
-        <br />
-        {/* <img
-            src="/1948-8-11x1952-3-3.webp"
-            alt="Image of the starCrossing for 1948-8-11x1952-3-3"
-            width={1200}
-            height={1200}
-          /> */}
-        <iframe
-          title="StarCrossed skyMap"
-          width="90%"
-          height="320"
-          src={starCrossedTest.value.skyMapURL}
-        ></iframe>
+        <Resource
+          value={starCrossedResource}
+          onPending={() => {
+            return (
+              <div>
+                <h2>
+                  StarCrossed Coordinates: <strong>Loading...</strong>
+                </h2>
+                <h3>Closest Star: Loading...</h3>
+                <p>Loading extra information about the closest star...</p>
+                <p>
+                  <Link
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="link margin1"
+                    href={`https://github.com/kaBeech/star-crossed/tree/main#readme`}
+                  >
+                    View README on GitHub
+                  </Link>
+                </p>
+                <h3>
+                  This is what it looks like in your own special corner of the
+                  universe:
+                </h3>
+                <p>Loading iframe...</p>
+                <br />
+                {/* <img
+          src="/1948-8-11x1952-3-3.webp"
+          alt="Image of the starCrossing for 1948-8-11x1952-3-3"
+          width={1200}
+          height={1200}
+        /> */}
+                <div class={"iframePlaceholder"}></div>
+              </div>
+            );
+          }}
+          onResolved={(starCrossedData) => {
+            return (
+              <div>
+                <h2>
+                  StarCrossed Coordinates:{" "}
+                  <strong>
+                    {starCrossedData.coordinates[0][0]}h{" "}
+                    {starCrossedData.coordinates[0][1]}m{" "}
+                    {starCrossedData.coordinates[0][2]}s,{" "}
+                    {starCrossedData.coordinates[1][0]}°{" "}
+                    {starCrossedData.coordinates[1][1]}′{" "}
+                    {starCrossedData.coordinates[1][2]}″
+                  </strong>
+                </h2>
+                <h3>Closest Star: {starCrossedData.closestStarShownName}</h3>
+                <p>
+                  <Link
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="link margin1"
+                    href={starCrossedData.infoURL}
+                  >
+                    Learn more about {starCrossedData.closestStarShownName}!
+                  </Link>
+                </p>
+                <p>
+                  <Link
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="link margin1"
+                    href={`https://github.com/kaBeech/star-crossed/tree/main#readme`}
+                  >
+                    View README on GitHub
+                  </Link>
+                </p>
+                <h3>
+                  This is what it looks like in your own special corner of the
+                  universe:
+                </h3>
+                <p>
+                  If the iframe below does not display, please allow insecure
+                  content in your browser or{" "}
+                  <Link
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="link"
+                    href={`http://www.wikisky.org/${starCrossedData.skyMapURL.slice(
+                      36
+                    )}`}
+                  >
+                    click here to view the interactive map on WikiSky
+                  </Link>
+                </p>
+                <br />
+                {/* <img
+              src="/1948-8-11x1952-3-3.webp"
+              alt="Image of the starCrossing for 1948-8-11x1952-3-3"
+              width={1200}
+              height={1200}
+            /> */}
+                <iframe
+                  title="StarCrossed skyMap"
+                  width="90%"
+                  height="24em"
+                  src={starCrossedData.skyMapURL}
+                ></iframe>{" "}
+              </div>
+            );
+          }}
+        />
+
         <p>
           <Link class="link margin1" href="../">
             {"<-- Back to Fun & Games"}
