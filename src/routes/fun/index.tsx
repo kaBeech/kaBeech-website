7d71597ee495aa5bdb4b322b155654444b81f435
kaBeech <contact@kaBeech.com>
2023-06-20 17:10:12 -0700

    Update wording

--- src/routes/fun/index.tsx
+++ src/routes/fun/index.tsx
@@ -59,8 +59,8 @@ export default component$(() => {
         <p>
           You can use it to check your compatibility with another person (based
           on your subjective opinion of what your stars look like), find a
-          galactic make-out spot, or just find a unique point in the sky to look
-          at together through your telescope
+          galactic make-out spot, or just discover a unique point in the sky to
+          look at together through your telescope
         </p>
         <h2>Buddh-A-Sketch</h2>
         <img
